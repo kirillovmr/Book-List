0f44a5998f9b2ecb4523b215778c2294a9496d14
Viktor Kirillov <mr.kirillov@icloud.com>
2018-11-14 18:12:37 -0600

    Added book detail component

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 const initialState = [
-  { title: 'JavaScript basics' },
-  { title: 'Harry Potter' },
-  { title: 'The Dark Tower' },
-  { title: 'Redux Book' }
+  { title: 'JavaScript basics', pages: 101 },
+  { title: 'Harry Potter', pages: 40 },
+  { title: 'The Dark Tower', pages: 35 },
+  { title: 'Redux Book', pages: 150 }
 ];
 
 export default function(state = initialState) {
